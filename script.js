1196a94474a9819cae041d323a17549aefa74be6
Yenkee <czekmasizsolt@gmail.com>
2026-06-27 18:25:25 +0200

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-function exitPage(){
+// ===============================
+// Doga
+// script.js
+// ===============================
 
+// Emlékezz a múltra! gomb
+const memoryButton = document.getElementById("memoryButton");
+
+memoryButton.addEventListener("click", function () {
+
+    // Később ide kerül majd az átirányítás.
+
+    // Példa:
+    // window.location.href = "mult.html";
+
+});
+
+
+// Exit gomb
+const exitButton = document.getElementById("exitButton");
+
+exitButton.addEventListener("click", function () {
+
+    // Megpróbálja bezárni az oldalt
     window.close();
 
+    // Ha nem sikerül, visszalép az előző oldalra
     history.back();
 
-}
+});
